a3309ab2c156b92200b9d6e7187cdd9ed195c18f
Alper Buluç <alper.buluc@gmail.com>
2022-06-21 21:34:21 +0300

    configure: rollup

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+// rollup.config.js
+import css from 'rollup-plugin-import-css';
+import { terser } from 'rollup-plugin-terser';
+import pkg from './package.json';
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      name: 'tepsi',
+      file: pkg.browser,
+      format: 'umd',
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+    },
+  ],
+  plugins: [css(), terser()],
+};
