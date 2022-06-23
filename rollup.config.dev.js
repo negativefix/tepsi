fba9558bdf3cebec5ff6575d42445e70c5d28682
Alper Buluç <alper.buluc@gmail.com>
2022-06-23 13:00:57 +0300

    update: split dev and prod config

---
+++ rollup.config.dev.js
@@ -0,0 +1,23 @@
+// rollup.config.js
+import babel from '@rollup/plugin-babel';
+import postcss from 'rollup-plugin-postcss';
+
+import path from 'path';
+
+// TOOD - Split dev/prodc configuration
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'dev/tepsi.js',
+    name: 'tepsi',
+    format: 'umd',
+  },
+  plugins: [
+    babel({ babelHelpers: 'bundled' }),
+    postcss({
+      extract: true,
+      extractPath: path.resolve(__dirname, 'dist/tepsi.min.css'),
+    }),
+  ],
+};
